63a15858e57ccf5f9ea1e3c0af1e6fe27c6b5726
Gabriel <gabriel.luthun@gmail.com>
2024-04-17 18:29:34 +0200

    Ajout de 2 boutons, et de divers fonctions de tri/filtrage

--- scripts/pieces.js
+++ scripts/pieces.js
@@ -1,34 +1,75 @@
-const response = await fetch ("pieces-autos.json");
-const pieces = await response.json();
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
 
-const article = pieces[0];
+for (let i = 0; i < pieces.length; i++) {
 
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "$" : "$$$"})`;
-
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie;
-
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description";
-
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+ }
 
 
+ const boutonTrier = document.querySelector(".btn-trier");
+ boutonTrier.addEventListener("click", () => {
+    const piecesOrdonnees = Array.from(pieces);
+    pieces.sort(function(a, b) {
+        return a.prix - b.prix;
+    })
+    console.log(pieces);
+    
+});
 
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+boutonFiltrer.addEventListener("click", () => {
+    const piecesFiltrees = pieces.filter(function(piece) {
+        return piece.prix <= 35;
+    });
+    console.log(piecesFiltrees);
+});
 
-//Rattachement des balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+//Ajout de boutons (html), de tri et de filtrage pour l'exercice
+const boutonDescription = document.querySelector(".btn-description");  
+boutonDescription.addEventListener("click", () => {
+    const piecesDescription = pieces.filter(function(piece) {
+        return piece.description;
+    });
+    console.log(piecesDescription);
+});
+
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", () => {
+    const piecesOrdonnees = Array.from(pieces);
+    const piecesDecroissant = pieces.sort(function(a, b) {
+        return b.prix - a.prix;
+    });
+    console.log(piecesDecroissant);
+});
